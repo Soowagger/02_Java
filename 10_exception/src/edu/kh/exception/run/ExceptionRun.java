ba33cffdc124c9cc4739a4e92b5b13491669e00d
Soowagger <soowagger@naver.com>
2024-02-19 17:45:44 +0900

    240219 컬렉션

---
+++ 10_exception/src/edu/kh/exception/run/ExceptionRun.java
@@ -0,0 +1,10 @@
+package edu.kh.exception.run;
+
+import edu.kh.exception.model.service.ExceptionService;
+
+public class ExceptionRun {
+	
+	public static void main(String[] args) {
+		new ExceptionService().ex4();
+	}
+}
---
+++ 11_collection/src/edu/kh/collection/run/StudentRun.java
@@ -0,0 +1,11 @@
+package edu.kh.collection.run;
+
+import edu.kh.collection.model.service.StudentService;
+
+public class StudentRun {
+	
+	public static void main(String[] args) {
+		//new StudentService().ex();
+		new StudentService().displayMenu();
+	}
+}
